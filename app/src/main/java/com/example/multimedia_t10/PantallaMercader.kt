69096d3178dc36675ec9ec5fa30e93162c5c3e83
AlumnoM <eduardoflores.9595@gmail.com>
2023-02-13 12:46:14 +0100

    t12 empezada

--- app/src/main/java/com/example/multimedia_t10/PantallaMercader.kt
+++ app/src/main/java/com/example/multimedia_t10/PantallaMercader.kt
@@ -1,9 +1,13 @@
 package com.example.multimedia_t10
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.core.view.isGone
 import com.example.multimedia_t10.databinding.ActivityMainBinding
 import com.example.multimedia_t10.databinding.ActivityPantallaMercaderBinding
@@ -20,20 +24,83 @@ class PantallaMercader : AppCompatActivity() {
 
         binding.button.setOnClickListener {
             binding.linear1.visibility = View.GONE
-            binding.button4.setOnClickListener {
+            binding.botonComprar.setOnClickListener {
                 binding.imageView.setImageResource(R.drawable.img_cosa_hoz)
+                binding.textView3.text = ""
+
+                //Revisar porque el boton mas y el menos tiene que ir en un linear
+                //para que aparezca sólo cuando se seleccione, mientras tanto el linea tiene que estar apagado.
+                val cambColBotMas = findViewById<Button>(R.id.botonMas)
+                val cambColBotMenos = findViewById<Button>(R.id.botonMenos)
+                findViewById<EditText>(R.id.textView3).setOnFocusChangeListener { view: View, bool: Boolean ->
+                    if (bool.equals(false)) {
+                        cambColBotMas.setBackgroundColor(0xFF000000.toInt())
+                        cambColBotMenos.setBackgroundColor(0xFF000000.toInt())
+                    } else {
+                        cambColBotMas.setBackgroundColor(0xFF000000.toInt())
+                        cambColBotMenos.setBackgroundColor(0xFF000000.toInt())
+                    }
+                }
+
+                findViewById<Button>(R.id.botonMas).setOnClickListener {
+                    metodoSumar(it)
+                }
+                findViewById<Button>(R.id.botonMenos).setOnClickListener {
+                    metodoRestar(it)
+                }
+
+
+
             }
 
+            binding.botonVender.setOnClickListener {
+
+            }
+
+            binding.botonCancelar.setOnClickListener {
+                val intent = Intent(this, PantallaMercader::class.java)
+                startActivity(intent)
+            }
         }
+
         binding.button2.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
 
-        binding.button6.setOnClickListener {
-            val intent = Intent(this, PantallaMercader::class.java)
-            startActivity(intent)
-        }
-
     }
+
+
+
+
+
+    @SuppressLint("SetTextI18n")
+    private fun metodoSumar(view: View) {
+        val numUsuario = 150
+        var numPantalla = findViewById<TextView>(R.id.textView3)
+
+        val nro1 = numUsuario
+        val nro2 = numPantalla.text.toString().toInt()
+        val suma = nro1 + nro2
+        numPantalla.text = "$suma"
+    }
+
+    private fun metodoRestar(view: View) {
+        val numUsuario = 150
+        val numPantalla = findViewById<TextView>(R.id.textView3)
+
+        val nro1 = numUsuario
+        val nro2 = numPantalla.text.toString().toInt()
+        val resta = nro2 - nro1
+        if (resta < 0) {
+            numPantalla.text = "0"
+        } else {
+            numPantalla.text = "$resta"
+        }
+    }
+
+
+
+
+
 }
