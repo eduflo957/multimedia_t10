8be16205fd72f8aabd36e7824b703b3a71343708
AlumnoM <eduardoflores.9595@gmail.com>
2023-02-21 12:41:25 +0100

    t12 empezada, la seguiré en la final, paralizo este proyecto.

--- app/src/main/java/com/example/multimedia_t10/PantallaVacia.kt
+++ app/src/main/java/com/example/multimedia_t10/PantallaVacia.kt
@@ -3,13 +3,11 @@ package com.example.multimedia_t10
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
 import com.example.multimedia_t10.databinding.ActivityPantallaVaciaBinding
 
 class PantallaVacia : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pantalla_vacia)
 
         val binding = ActivityPantallaVaciaBinding.inflate(layoutInflater)
         setContentView(binding.root)
